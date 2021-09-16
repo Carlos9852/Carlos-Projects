9bb1247d3f03e229e2fa35d6706e339140ee9c92
Carlos9852 <87607986+Carlos9852@users.noreply.github.com>
2021-09-15 21:23:18 -0300

    Update Automatic Irrigation.ino

--- Automatic-Irrigation/Automatic Irrigation.ino
+++ Automatic-Irrigation/Automatic Irrigation.ino
@@ -36,7 +36,37 @@ void setup(){
   pinMode(relayPin,   OUTPUT);
   digitalWrite(relayPin, LOW);
   
-} /* end main */
+} /* end setup */
+
+
+/* ========================================================================= */
+/* --- Loop Infinito --- */
+void loop ()
+{
+  DateTime agora = rtc.now();                                  //Faz a leitura de dados de data e hora
+  Serial.print("Data: ");                                      //Imprime "data"
+  Serial.print(agora.day(), DEC);                              //Imprime dia
+  Serial.print('/');                                           //Imprime barra
+  Serial.print(agora.month(), DEC);                            //Imprime mes
+  Serial.print('/');                                           //Imprime barra
+  Serial.print(agora.year(), DEC);                             //Imprime ano
+  Serial.print(" / Dia da semana: ");                          //Imprime texto
+  Serial.print(diasDaSemana[agora.dayOfTheWeek()]);            //Imprime dia da semana
+  Serial.print(" / Horas: ");                                  //Imprime texto
+  Serial.print(agora.hour(), DEC);                             //Imprime hora
+  Serial.print(':');                                           //Imprime dois pontos
+  Serial.print(agora.minute(), DEC);                           //Imprime os minutos
+  Serial.print(':');                                           //Imprime dois pontos
+  Serial.print(agora.second(), DEC);                           //Imprime os segundos
+  Serial.println();                                            //Quebra de linha
+  delay(1000);                                                 //1 Segundo
+
+  if ((agora.hour() == HORA) && (agora.minute() == MINUTO)) {  //se no instante que hora atual for igual a hora da variavel
+    digitalWrite(PINO_RELE, HIGH);                             //aciona rele
+  } else {                                                     //senao
+    digitalWrite(PINO_RELE, LOW);                              //desaciona o rele
+  }
+} /* end loop */
 
 
 /* ============================================================================  
