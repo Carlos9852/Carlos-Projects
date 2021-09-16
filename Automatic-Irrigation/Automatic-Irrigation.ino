0308e14e137de9abb34febb3b5308494bf28e21b
Carlos9852 <87607986+Carlos9852@users.noreply.github.com>
2021-09-16 14:34:27 -0300

    Update Automatic-Irrigation.ino

--- Automatic-Irrigation/Automatic-Irrigation.ino
+++ Automatic-Irrigation/Automatic-Irrigation.ino
@@ -1,38 +1,59 @@
 /* ============================================================================
-
     Automatic Irrigation
     
     Autor: Carlos Daniel de Souza Nunes
     Data: 15/09/2021
-
 ============================================================================ */
 
 
 /* ========================================================================= */
 /* --- Bibliotecas --- */
+#include <Arduino.h>
 #include <Wire.h>
 #include "RTClib.h"
+#include <LiquidCrystal.h>
 
 
 /* ========================================================================= */
-/* --- Variaveis Globais --- */
-const int ralayPin =   2,        //pino definido para conexao com o rele
-          Hour1     =  7,        //variavel de hora que o rele ira acionar
-          Minute1   = 30,        //variavel de minuto que o rele ira acionar
-          Second1   =  0;        //variavel de segundo que o rele ira acionar
+/* --- Constantes e Variaveis Globais --- */
+#define sensorUmid A0  
+const int relayPin   =   2,        //pino definido para conexao com o rele
+          Hour1      =  14,        //variavel de hora que o rele ira acionar
+          Minute1    =  33,        //variavel de minuto que o rele ira acionar
+          Second1    =   0,        //variavel de segundo que o rele ira acionar
+          tempoRega  =  2 * 60000;
+
+unsigned long delay1 = 0,
+              delay2 = 0;
 
 bool      logicRelay = false;
 
 
 /* ========================================================================= */
+/* --- Protótipos das Funções --- */
+void lcdPrint();
+void irrigar();
+void solo();
+
+
+/* ========================================================================= */
+/* --- Instâncias --- */
+//As linhas de codigo a seguir devem ser comentadas, ou descomentadas, de acordo com o modelo de RTC utilizado (DS1307 ou DS3132)
+//RTC_DS1307 rtc; //Objeto rtc da classe DS1307
+RTC_DS3231 rtc;   //Objeto rtc da classe DS3132
+      
+char diasDaSemana[7][12] = {"Domingo", "Segunda", "Terca", "Quarta", "Quinta", "Sexta", "Sabado"}; //Dias da semana
+
+
+/* ========================================================================= */
 /* --- Função Principal --- */
 void setup(){
-  Serial.begin(9600);                              //Inicializa a comunicacao serial
+  Serial.begin(115200);                              //Inicializa a comunicacao serial
   if (!rtc.begin()) {                              //Se o RTC nao for inicializado, faz
     Serial.println("RTC NAO INICIALIZADO");        //Imprime o texto
     while (1);                                     //Trava o programa
   }
-  //rtc.adjust(DateTime(2019, 3, 18, 10, 53, 00)); //Ajusta o tempo do RTC para a data e hora definida pelo usuario.
+  //rtc.adjust(DateTime(2021, 9, 21, 13, 21, 00)); //Ajusta o tempo do RTC para a data e hora definida pelo usuario.
   delay(100);                                      //100 Milissegundos
 
   pinMode(relayPin,           OUTPUT);
@@ -45,23 +66,53 @@ void setup(){
 /* --- Loop Infinito --- */
 void loop ()
 {
+  DateTime agora = rtc.now();
+
+  if((millis() - delay1) > 1000){
+    delay1 = millis();
+    lcdPrint();
+    solo();
+  }
+} /* end loop */
+
+
+/* ========================================================================= */
+/* --- Outras Funções --- */
+void lcdPrint(){
   DateTime agora = rtc.now();                                  //Faz a leitura de dados de data e hora
-  Serial.print("Horas: ");                                     //Imprime texto
+
+  Serial.print("Hora: ");                                      //Imprime texto
   Serial.print(agora.hour(), DEC);                             //Imprime hora
   Serial.print(':');                                           //Imprime dois pontos
   Serial.print(agora.minute(), DEC);                           //Imprime os minutos
   Serial.print(':');                                           //Imprime dois pontos
-  Serial.print(agora.second(), DEC);                           //Imprime os segundos
-  Serial.println();                                            //Quebra de linha
-  delay(1000);                                                 //1 Segundo
+  Serial.print(agora.second(), DEC);                         //Imprime os segundos
+  
+} /* end lcdPrint */
 
+void solo(){
+  int valor, 
+      umid;
+  
+  valor = analogRead(sensorUmid);
+  DateTime agora = rtc.now();
+  umid = map(valor, 0, 1023, 100, 0);
+
+  Serial.print("  ");
+  Serial.print("Umidade: ");
+  Serial.print(umid);
+  Serial.println('%');
+  
   if ((agora.hour() == Hour1) && (agora.minute() == Minute1) && (agora.second() == Second1)) {  //se no instante que hora atual for igual a hora da variavel
-    digitalWrite(relayPin,  logicRelay);                                                        //aciona rele
-  } else {                                                                                      //senao
-    digitalWrite(relayPin, !logicRelay);                                                        //desaciona o rele
+    delay2 = millis();
+    if (umid <= 70){
+      while((millis() - delay2) < tempoRega){
+        digitalWrite(relayPin,  logicRelay);
+      }
+    }
+      digitalWrite(relayPin, !logicRelay);
   }
-} /* end loop */
-
+}
 
 /* ============================================================================  
                                                               
