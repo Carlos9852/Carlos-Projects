a300553ac4023f44db07d14396387747186308e5
Carlos9852 <87607986+Carlos9852@users.noreply.github.com>
2021-09-18 01:31:45 -0300

    Update Automatic-Irrigation.ino

--- Automatic-Irrigation/Automatic-Irrigation.ino
+++ Automatic-Irrigation/Automatic-Irrigation.ino
@@ -1,5 +1,5 @@
 /* ============================================================================
-    Automatic Irrigation
+    Smart Watering
     
     Autor: Carlos Daniel de Souza Nunes
     Data: 15/09/2021
@@ -17,30 +17,26 @@
 /* ========================================================================= */
 /* --- Constantes e Variaveis Globais --- */
 #define sensorUmid A0  
-const int rs = D7, en = D6, d4 = D5, d5 = D4, d6 = D3, d7 = D0,
-          relayPin   =  D8,        //pino definido para conexao com o rele
-          Hour1      =   0,        //variavel de hora que o rele ira acionar
-          Minute1    =  25,        //variavel de minuto que o rele ira acionar
-          Second1    =   0,        //variavel de segundo que o rele ira acionar
-          Hour2      =   0,        //variavel de hora que o rele ira acionar
-          Minute2    =  31,        //variavel de minuto que o rele ira acionar
-          Second2    =   0;        //variavel de segundo que o rele ira acionar
+const int rs = D7, en = D6, d4 = D5, d5 = D4, d6 = D3, d7 = D0,      //variaveis dos pinos do lcd
+          relayPin   =  D8,                                //pino definido para conexao com o rele
+          Hour1      =   7,                                //variavel de hora1 que o rele ira acionar
+          Minute1    =  30,                                //variavel de minuto1 que o rele ira acionar
+          Second1    =  19,                                //variavel de segundo1 que o rele ira acionar
+          Hour2      =   0,                                //variavel de hora2 que o rele ira acionar
+          Minute2    =  30,                                //variavel de minuto2 que o rele ira acionar
+          Second2    =   0;                                //variavel de segundo2 que o rele ira acionar
 
-int       tempoRega  =   2,
-          valor, 
-          umid;
+int       tempoRega  =   2;                                //variavel para o tempo de que o rele vai ficar ligado
+          
+unsigned long delay1 = 0;                                  //variavel para o uso da millis();
 
-unsigned long delay1 = 0,
-              delay2 = 0;
-
-bool      logicRelay = true;
+bool      logicRelay = true;                               //variavel que controla a lógica do rele
 
 
 /* ========================================================================= */
 /* --- Protótipos das Funções --- */
-void lcdPrint();
-void irrigar();
-void solo();
+void lcdPrint();                                           //Protótipo da função lcdPrint.
+void solo();                                               //Protótipo da função solo.
 
 
 /* ========================================================================= */
@@ -56,25 +52,25 @@ char diasDaSemana[7][12] = {"Domingo", "Segunda", "Terca", "Quarta", "Quinta", "
 /* ========================================================================= */
 /* --- Função Principal --- */
 void setup(){
-  Serial.begin(115200);                              //Inicializa a comunicacao serial
-  lcd.begin(16, 2);
-  lcd.setCursor(1, 0);
-  lcd.print("Smart Watering");
-  lcd.setCursor(4, 1);
-  lcd.print("By Carlos");
-  delay(5000);
-  if (!rtc.begin()) {                              //Se o RTC nao for inicializado, faz
-    lcd.clear();
-    lcd.print("RTC NAO INICIOU");        //Imprime o texto
-    while (1);                                     //Trava o programa
+  Serial.begin(115200);                                    //Inicializa a comunicacao serial
+  lcd.begin(16, 2);                                        //Inicializa o LCD 16x2
+  lcd.setCursor(1, 0);                                     //Define o cursor para a coluna 2 e linha 1
+  lcd.print("Smart Watering");                             //Imprime a "Smart Watering" no display
+  lcd.setCursor(4, 1);                                     //Define o cursor para a coluna 5 e linha 2
+  lcd.print("By Carlos");                                  //Imprime "By Carlos" no LCD
+  delay(5000);                                             //Espera 5 segundos
+  if (!rtc.begin()) {                                      //RTC não foi inicializado? Sim.
+    lcd.clear();                                           //Limpa o LCD
+    lcd.print("RTC NAO INICIOU");                          //Imprime o texto no LCD
+    while (1);                                             //Trava o programa
   }
-  //rtc.adjust(DateTime(2021, 9, 21, 13, 21, 00)); //Ajusta o tempo do RTC para a data e hora definida pelo usuario.
-  delay(100);                                      //100 Milissegundos
+  //rtc.adjust(DateTime(2021, 9, 21, 13, 21, 00));         //Ajusta o tempo do RTC para a data e hora definida pelo usuario.
+  delay(100);                                              //Espera 100 Milissegundos
   
-  tempoRega = tempoRega * 60;
+  tempoRega = tempoRega * 60;                              //Ajusta o tempo para rega de minutos para segundos
 
-  pinMode(relayPin,           OUTPUT);
-  digitalWrite(relayPin, !logicRelay);
+  pinMode(relayPin,           OUTPUT);                     //Define o pino do rele como saida
+  digitalWrite(relayPin, !logicRelay);                     //Inicia o pino do rele como desligado
   
 } /* end setup */
 
@@ -95,71 +91,71 @@ void loop ()
 /* ========================================================================= */
 /* --- Outras Funções --- */
 void lcdPrint(){
-  DateTime agora = rtc.now();                                  //Faz a leitura de dados de data e hora
+  DateTime agora = rtc.now();                              //Faz a leitura de dados de data e hora
 
-  valor = analogRead(sensorUmid);
-  umid = map(valor, 0, 1023, 100, 0);
   lcd.clear();
-  lcd.print("Hora: ");                                      //Imprime texto
-  lcd.print(agora.hour(), DEC);                             //Imprime hora
-  lcd.print(':');                                           //Imprime dois pontos
-  lcd.print(agora.minute(), DEC);                           //Imprime os minutos
-  lcd.print(':');                                           //Imprime dois pontos
-  lcd.print(agora.second(), DEC);                         //Imprime os segundos
+  lcd.print("Hora: ");                                     //Imprime texto
+  lcd.print(agora.hour(), DEC);                            //Imprime hora
+  lcd.print(':');                                          //Imprime dois pontos
+  lcd.print(agora.minute(), DEC);                          //Imprime os minutos
+  lcd.print(':');                                          //Imprime dois pontos
+  lcd.print(agora.second(), DEC);                          //Imprime os segundos
   
 } /* end lcdPrint */
 
 void solo(){
 
-  static bool aux;
+  static bool aux;                                         //Variavel auxiliar. OBS: é necessária?
+  int       valor,                                         //Variavel para armazenar o valor lido pela porta analógica
+             umid;                                         //Variavel que converte o valor SensorUmid para porcentagem
   
-  valor = analogRead(sensorUmid);
-  DateTime agora = rtc.now();
-  umid = map(valor, 0, 1023, 100, 0);
+  valor = analogRead(sensorUmid);                          //Faz a leitura da porta analógica e salva o valor em "valor"
+  DateTime agora = rtc.now();                              //Faz a leitura da horario
+  umid = map(valor, 0, 1023, 100, 0);                      //Mapeia a variavel valor para umid
   
-  lcd.setCursor(0,1);
-  lcd.print("Umidade: ");
-  lcd.print(umid);
-  lcd.print('%');
+  lcd.setCursor(0,1);                                      //Define o cursor para coluna 1 e linha 2
+  lcd.print("Umidade: ");                                  //Imprime o texto no LCD
+  lcd.print(umid);                                         //Imprime o valor de umid no LCD
+  lcd.print('%');                                          //Imprime o caracter '%' no LCD
 
   if (((agora.hour() == Hour1) && (agora.minute() == Minute1) && (agora.second() == Second1)) || ((agora.hour() == Hour2) && (agora.minute() == Minute2) && (agora.second() == Second2))) {  //se no instante que hora atual for igual a hora da variavel
-    lcd.clear();
-    lcd.setCursor(3, 0);
-    lcd.print("Analisando!");
-    delay(5000);
-    if(umid <= 70){
-      lcd.clear();
-      lcd.setCursor(3, 0);
-      lcd.print("Solo seco!");
-      delay(5000);
-      for(int i = 0; i <= tempoRega; i++){
-        digitalWrite(relayPin, logicRelay);
-        lcdPrint();
-        lcd.setCursor(0, 1);
-        lcd.print("Regando");
-        for(int i = 0; i < 3; i++){
-          lcd.print('.');
-          delay(333);
+    lcd.clear();                                           //Limpa a tela do LCD
+    lcd.setCursor(3, 0);                                   //Define o cursor para a linha 4 e coluna 1
+    lcd.print("Analisando!");                              //Imprime o texto no LCD
+    delay(5000);                                           //Espera por 5 segundos
+    if(umid <= 70){                                        //A umidade é menor que 70%? sim?
+      lcd.clear();                                         //Limpa a tela do LCD
+      lcd.setCursor(3, 0);                                 //Define o cursor para a coluna 4 e linha 1
+      lcd.print("Solo seco!");                             //Imprime o Texto no LCD
+      delay(5000);                                         //Espera por 5 segundos
+      for(int i = 0; i <= tempoRega; i++){                 //Para i igual a 0, menor que o tempoRega incrementa em um
+        digitalWrite(relayPin, logicRelay);                //Liga o rele
+        lcdPrint();                                        //Chama a função lcdPrint
+        lcd.setCursor(0, 1);                               //Define o cursor para a coluna 1 e linha 2
+        lcd.print("Regando");                              //Imprime o texto no LCD
+        for(int i = 0; i < 3; i++){                        //Para i igual a 0, i menor que 3, i incrementa em um
+          lcd.print('.');                                  //Imprime um caracter no LCD
+          delay(333);                                      //Espera 333 milisegundos
         }
         aux = true;
       }
-      lcd.clear();
-      lcd.print("Solo Regado");
-      delay(5000);
+      lcd.clear();                                         //Limpa a tela do LCD
+      lcd.print("Solo Regado");                            //Imprime o texto na tela do LCD
+      delay(5000);                                         //Espera por 5 segundos
     }
-    else{
-      lcd.clear();
-      lcd.setCursor(3, 0);
-      lcd.print("Solo umido!");
-      delay(5000);
+    else{                                                  //A umidade está menor que 70%? Não?
+      lcd.clear();                                         //Limpa a tela do LCD
+      lcd.setCursor(3, 0);                                 //Define o cursor para a coluna 4 e linha 1
+      lcd.print("Solo umido!");                            //Imprime o texto na tela
+      delay(5000);                                         //Espera por 5 segundos
     }
   }
 
-  if(aux == true){
-    aux = false;
-    digitalWrite(relayPin, !logicRelay);
+  if(aux == true){                                         //A variavel aux está como high? Sim?
+    aux = false;                                           //Define aux com low
+    digitalWrite(relayPin, !logicRelay);                   //Desliga o rele
   }
-}
+} /* end solo */
 
 /* ============================================================================  
                                                               
